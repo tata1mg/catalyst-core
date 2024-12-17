78243c45f12f66c4849b8e53065b88738ff2540a
mayankmahavar1mg <mayank.mahavar@1mg.com>
2024-12-18 00:44:11 +0530

    fix: cmake file

--- src/native/androidProject/app/build.gradle.kts
+++ src/native/androidProject/app/build.gradle.kts
@@ -59,13 +59,6 @@ android {
         jvmTarget = "1.8"
     }
 
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
-
     buildFeatures {
         viewBinding = true
         buildConfig = true
