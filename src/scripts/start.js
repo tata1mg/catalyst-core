a6d99989f0799e478b88f2c7a30ae344b8b92a04
Mayan Sharma <mayansharma@G206F1WHVF.local>
2025-01-27 16:36:10 +0530

    refactor: split start commands for windows (separate spawn)

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,5 +1,5 @@
 const path = require("path")
-const { spawnSync } = require("child_process")
+const { spawnSync, spawn } = require("child_process")
 const { arrayToObject } = require("./scriptUtils")
 const { name } = require(`${process.cwd()}/package.json`)
 const { BUILD_OUTPUT_PATH } = require(`${process.cwd()}/config/config.json`)
@@ -19,23 +19,59 @@ function start() {
     `
 
     if (isWindows) {
-        command = `node ./dist/scripts/checkVersion && start /b npx babel-node -r ./dist/scripts/loadScriptsBeforeServerStarts.js ./dist/webpack/development.client.babel --no-warnings=ExperimentalWarning --no-warnings=BABEL && start /b npx babel-node -r ./dist/scripts/loadScriptsBeforeServerStarts.js ./dist/server/startServer.js --watch-path=${process.cwd()}/server --watch-path=${process.cwd()}/src --ignore='__IGNORE__' --no-warnings=ExperimentalWarning --no-warnings=BABEL`
-    }
+        spawn(
+            `node ./dist/scripts/checkVersion && start /b npx babel-node -r ./dist/scripts/loadScriptsBeforeServerStarts.js ./dist/webpack/development.client.babel --no-warnings=ExperimentalWarning --no-warnings=BABEL`,
+            [],
+            {
+                cwd: dirname,
+                stdio: "inherit",
+                shell: true,
+                env: {
+                    ...process.env,
+                    src_path: process.cwd(),
+                    NODE_ENV: "development",
+                    IS_DEV_COMMAND: false,
+                    APPLICATION: name || "catalyst_app",
+                    BUILD_OUTPUT_PATH: BUILD_OUTPUT_PATH,
+                    ...argumentsObject,
+                },
+            }
+        )
 
-    spawnSync(command, [], {
-        cwd: dirname,
-        stdio: "inherit",
-        shell: true,
-        env: {
-            ...process.env,
-            src_path: process.cwd(),
-            NODE_ENV: "development",
-            IS_DEV_COMMAND: false,
-            APPLICATION: name || "catalyst_app",
-            BUILD_OUTPUT_PATH: BUILD_OUTPUT_PATH,
-            ...argumentsObject,
-        },
-    })
+        spawn(
+            `node ./dist/scripts/checkVersion && npx babel-node -r ./dist/scripts/loadScriptsBeforeServerStarts.js ./dist/server/startServer.js --watch-path=${process.cwd()}/server --watch-path=${process.cwd()}/src --ignore='__IGNORE__' --no-warnings=ExperimentalWarning --no-warnings=BABEL`,
+            [],
+            {
+                cwd: dirname,
+                stdio: "inherit",
+                shell: true,
+                env: {
+                    ...process.env,
+                    src_path: process.cwd(),
+                    NODE_ENV: "development",
+                    IS_DEV_COMMAND: false,
+                    APPLICATION: name || "catalyst_app",
+                    BUILD_OUTPUT_PATH: BUILD_OUTPUT_PATH,
+                    ...argumentsObject,
+                },
+            }
+        )
+    } else {
+        spawnSync(command, [], {
+            cwd: dirname,
+            stdio: "inherit",
+            shell: true,
+            env: {
+                ...process.env,
+                src_path: process.cwd(),
+                NODE_ENV: "development",
+                IS_DEV_COMMAND: false,
+                APPLICATION: name || "catalyst_app",
+                BUILD_OUTPUT_PATH: BUILD_OUTPUT_PATH,
+                ...argumentsObject,
+            },
+        })
+    }
 }
 
 start()
