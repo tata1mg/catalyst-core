698a62c81ce3e940167c4947e92427f9b81848d3
utkarsh-1mg <utkarsh.srivastava@1mg.com>
2025-09-05 18:34:04 +0530

    fix: add unhandled rejection handler

--- src/server/startServer.js
+++ src/server/startServer.js
@@ -37,10 +37,12 @@ process.on("message", function (msg) {
     }
 })
 
-if (env === "development") {
-    // Add better stack tracing for promises in dev mode
-    process.on("unhandledRejection", (r) => console.debug(r))
-}
+// if (env === "development") {
+//     // Add better stack tracing for promises in dev mode
+//     process.on("unhandledRejection", (r) => console.debug(r))
+// }
+ // Add better stack tracing for promises in dev mode
+process.on("unhandledRejection", (err) => console.log("unhandledRejection in Catalyst", err))
 
 const port = process.env.NODE_SERVER_PORT ?? 3005
 const host = process.env.NODE_SERVER_HOSTNAME ?? "localhost"
