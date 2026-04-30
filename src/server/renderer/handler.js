bdbbb2b664802bb68a1063f891e05ecc21014bf3
D-Agam <agamdawra01@gmail.com>
2026-04-30 14:04:38 +0530

    added comment for isBot

--- src/server/renderer/handler.js
+++ src/server/renderer/handler.js
@@ -236,6 +236,7 @@ const renderMarkUp = async (
     webExtractor
 ) => {
     const deviceDetails = getUserAgentDetails(req.headers["user-agent"] || "")
+    // Keep bot detection in source system and pass isBot through the request for reuse.
     const isBot = req.isBot ?? deviceDetails.googleBot || deviceDetails.aiBot ? true : false
 
     let state = store.getState()
