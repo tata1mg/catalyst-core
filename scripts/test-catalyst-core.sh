5172e1ee8189652f1a826072338aa4281b172255
Mayan Sharma <mayan.sharma@1mg.com>
2026-06-30 16:23:04 +0530

    fix: sync fixture catalyst-core bin

--- scripts/test-catalyst-core.sh
+++ scripts/test-catalyst-core.sh
@@ -20,7 +20,9 @@ npm run prepare --workspace packages/catalyst-core
 
 # Replace built catalyst-core in the fixture app
 rm -rf "$FIXTURE_DIR/node_modules/catalyst-core/dist"
+rm -rf "$FIXTURE_DIR/node_modules/catalyst-core/bin"
 mv "$PACKAGE_DIR/dist" "$FIXTURE_DIR/node_modules/catalyst-core/"
+cp -R "$PACKAGE_DIR/bin" "$FIXTURE_DIR/node_modules/catalyst-core/"
 cp "$PACKAGE_DIR/package.json" "$FIXTURE_DIR/node_modules/catalyst-core/package.json"
 
 # Run fixture app checks
